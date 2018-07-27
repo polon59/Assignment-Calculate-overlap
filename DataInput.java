a9aeccc9bcd99760e2127fd84ff7623977965853
polon59 <przemyslaw.polczak@gmail.com>
2018-07-27 20:22:48 +0200

    implement method 'getOptionNumber' to get menu option from users input

--- DataInput.java
+++ DataInput.java
@@ -11,26 +11,39 @@ public class DataInput {
 
 
     public int[] getCoordinatesFromInput(){
+
         Scanner input = new Scanner(System.in);
         usersCoordinates = input.next();
-        input.close();
         splitUsersCoordinates();
         convertUsersCoordinatesToInt();
 
         return convertedCoordinates;
-
     }
 
+
     private void splitUsersCoordinates(){
+
         splittedUsersCoordinates = usersCoordinates.split(",");
     }
 
+
     private void convertUsersCoordinatesToInt(){
+
         convertedCoordinates = new int[8];
 
         for (int i = 0; i < splittedUsersCoordinates.length; i++){
             convertedCoordinates[i] = Integer.valueOf(splittedUsersCoordinates[i]);
         }
+    }
 
+
+    public int getOptionNumber(){
+
+        Scanner usersChoice = new Scanner(System.in);
+        // use class wiew
+        System.out.println("Select option");
+        int option = usersChoice.nextInt();
+
+        return option;
     }
 }
