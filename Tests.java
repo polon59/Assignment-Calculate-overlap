eb2f6679f2f50130b4554cd92e8d16da7b9db6fa
polon59 <przemyslaw.polczak@gmail.com>
2018-07-30 23:48:35 +0200

    change acces modifier in Ansi color variable

--- Tests.java
+++ Tests.java
@@ -7,7 +7,7 @@ public class Tests {
     private final String ANSI_BLACK = "\u001B[30m";
     private final String ANSI_RED = "\u001B[31m";
     private final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_CYAN = "\u001B[36m";
+    private final String ANSI_CYAN = "\u001B[36m";
 
 
     public Tests(Calculations calculations){
