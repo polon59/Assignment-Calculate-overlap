7867cc66051ad86e3731f67a6ab92bc077f760b4
polon59 <przemyslaw.polczak@gmail.com>
2018-07-30 22:39:34 +0200

    implement method 'setInitialCoordinatesValues()' in class 'Display'

--- Display.java
+++ Display.java
@@ -1,5 +1,30 @@
 
 public class Display {
 
+    int[] coordinatesDisplay;
     
+    public Display(){
+        coordinatesDisplay = new int[8];
+    }
+
+
+    private void setInitialCoordinatesValues(){
+        int i = 0;
+
+        while(i < coordinatesDisplay.length){
+            coordinatesDisplay[i] = "x =   ,";
+            i++;
+            coordinatesDisplay[i] = "y =    ";
+        }
+    }
+
+    private void resetIndexesColors(){
+        
+    }
+
+
+    public void displayCoordinatesImage(int currentIndex){
+        coordinatesDisplay[currentIndex] = 
+        System.out.println();
+    }
 }
